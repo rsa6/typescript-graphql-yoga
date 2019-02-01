f8c7bdb1a2ac1aef04256a96ab5f868c578316f1
rsa6 <rsa6@naver.com>
2019-02-02 08:29:04 +0900

    last

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -3,12 +3,12 @@ const resolvers = {
     hello: (_: any, { name }: any) => `Hello ${name || 'World'}`,
     input: (root: any, args: any, context: any, info: any) => {
       console.log(`3. resolver: hello`);
-      return `Hello ${args.name ? args.name : 'world'}!`
+      return `Hello ${args.name ? args.name : 'world'}`
     },
     
     output: (root: any, args: any, context: any, info: any) => {
       console.log(`3. resolver: bye`);
-      return `Bye ${args.name ? args.name : 'world'}!`
+      return `Bye ${args.name ? args.name : 'world'}`
     },
   },
   Mutation: {
